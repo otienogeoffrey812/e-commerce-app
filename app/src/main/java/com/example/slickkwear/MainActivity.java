1f6ada00eaa5ae1bd5d8857c7a75128e56f8b25d
Geoffrey Otieno <otienogeoffrey812@gmail.com>
2021-02-22 23:30:57 +0300

    Updated

--- app/src/main/java/com/example/slickkwear/MainActivity.java
+++ app/src/main/java/com/example/slickkwear/MainActivity.java
@@ -2,30 +2,17 @@ package com.example.slickkwear;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
-import android.content.Intent;
-import android.graphics.Color;
+
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.example.slickkwear.Model.Products;
-import com.example.slickkwear.Model.SliderItem;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
-import com.smarteist.autoimageslider.SliderAnimations;
 import com.smarteist.autoimageslider.SliderView;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView bottomNavigationView;
     SliderView sliderView;
@@ -43,68 +30,94 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         query = productRef.collection("Products");
 
          bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
-         bottomNavigationView.setOnNavigationItemSelectedListener(this);
+         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new UserHomeFragment())
+                .commit();
 
 
-        homeSliderBanner();
+//        homeSliderBanner();
     }
 
-    private void homeSliderBanner() {
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
 
-        sliderView = findViewById(R.id.imageSlider);
-
-        adapter = new SliderHomeAdapter(this);
-
-        query.get().addOnSuccessListener(
-                new OnSuccessListener<QuerySnapshot>() {
-                    @Override
-                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-
-                        for (QueryDocumentSnapshot result : queryDocumentSnapshots)
-                        {
-                            SliderItem sliderItem = new SliderItem();
-                            sliderItem.setDescription(result.getString("ProductName"));
-                            sliderItem.setImageUrl(result.getString("ProductImage"));
-                            adapter.addItem(sliderItem);
-                        }
-
-                    }
-                }
-        );
-
-        sliderView.setSliderAdapter(adapter);
-
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
-        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
-        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
-        sliderView.setIndicatorSelectedColor(Color.WHITE);
-//        sliderView.setIndicatorUnselectedColor(R.attr.colorPrimary);
-        sliderView.setScrollTimeInSec(3);
-        sliderView.setAutoCycle(true);
-        sliderView.startAutoCycle();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId() == R.id.bottom_nav_home)
-        {
-            Toast.makeText(this, "Home Clicked", Toast.LENGTH_SHORT).show();
+            switch (item.getItemId())
+            {
+                case R.id.bottom_nav_home:
+                    selectedFragment = new UserHomeFragment();
+                    break;
+                case R.id.bottom_nav_category:
+                    selectedFragment = new UserCategoryFragment();
+                    break;
+                case R.id.bottom_nav_cart:
+                    selectedFragment = new UserCartFragment();
+                    break;
+                case R.id.bottom_nav_account:
+                    selectedFragment = new UserAccountFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+            return true;
         }
-        else if (item.getItemId() == R.id.bottom_nav_category)
-        {
-            Intent intent = new Intent(getApplicationContext(), AdminOrdersActivity.class);
-            startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.bottom_nav_cart)
-        {
-            Intent intent = new Intent(getApplicationContext(), AdminCategoriesActivity.class);
-            startActivity(intent);
-        }
-        else if (item.getItemId() == R.id.bottom_nav_account)
-        {
-            Intent intent = new Intent(getApplicationContext(), AdminCategoriesActivity.class);
-            startActivity(intent);
-        }
-        return false;
-    }
+    };
+
+//    private void homeSliderBanner() {
+//
+//        sliderView = findViewById(R.id.imageSlider);
+//
+//        adapter = new SliderHomeAdapter(this);
+//
+//        query.get().addOnSuccessListener(
+//                new OnSuccessListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//
+//                        for (QueryDocumentSnapshot result : queryDocumentSnapshots)
+//                        {
+//                            SliderItem sliderItem = new SliderItem();
+//                            sliderItem.setDescription(result.getString("ProductName"));
+//                            sliderItem.setImageUrl(result.getString("ProductImage"));
+//                            adapter.addItem(sliderItem);
+//                        }
+//
+//                    }
+//                }
+//        );
+//
+//        sliderView.setSliderAdapter(adapter);
+//
+//        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+//        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+//        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
+//        sliderView.setIndicatorSelectedColor(Color.WHITE);
+////        sliderView.setIndicatorUnselectedColor(R.attr.colorPrimary);
+//        sliderView.setScrollTimeInSec(3);
+//        sliderView.setAutoCycle(true);
+//        sliderView.startAutoCycle();
+//    }
+//
+//    @Override
+//    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//        if(item.getItemId() == R.id.bottom_nav_home)
+//        {
+//            Toast.makeText(this, "Home Clicked", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (item.getItemId() == R.id.bottom_nav_category)
+//        {
+//            Toast.makeText(this, "Category Clicked", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (item.getItemId() == R.id.bottom_nav_cart)
+//        {
+//            Toast.makeText(this, "Cart Clicked", Toast.LENGTH_SHORT).show();
+//        }
+//        else if (item.getItemId() == R.id.bottom_nav_account)
+//        {
+//            Toast.makeText(this, "Account Clicked", Toast.LENGTH_SHORT).show();
+//        }
+//        return true;
+//    }
 }
---
+++ app/src/main/java/com/example/slickkwear/UserHomeFragment.java
@@ -0,0 +1,92 @@
+package com.example.slickkwear;
+
+import android.graphics.Color;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.slickkwear.Model.SliderItem;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.smarteist.autoimageslider.IndicatorView.animation.type.IndicatorAnimationType;
+import com.smarteist.autoimageslider.SliderAnimations;
+import com.smarteist.autoimageslider.SliderView;
+
+public class UserHomeFragment extends Fragment {
+
+
+    private BottomNavigationView bottomNavigationView;
+    SliderView sliderView;
+    private SliderHomeAdapter adapter;
+
+    private FirebaseFirestore productRef;
+    private Query query;
+
+
+    public UserHomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_user_home, container, false);
+
+        sliderView = view.findViewById(R.id.imageSlider);
+
+        productRef = FirebaseFirestore.getInstance();
+        query = productRef.collection("Products");
+
+        homeSliderBanner();
+
+        return  view;
+    }
+
+    private void homeSliderBanner() {
+
+        adapter = new SliderHomeAdapter(requireContext());
+
+        query.get().addOnSuccessListener(
+                new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                        for (QueryDocumentSnapshot result : queryDocumentSnapshots)
+                        {
+                            SliderItem sliderItem = new SliderItem();
+                            sliderItem.setDescription(result.getString("ProductName"));
+                            sliderItem.setImageUrl(result.getString("ProductImage"));
+                            adapter.addItem(sliderItem);
+                        }
+
+                    }
+                }
+        );
+
+        sliderView.setSliderAdapter(adapter);
+
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
+        sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_RIGHT);
+        sliderView.setIndicatorSelectedColor(Color.WHITE);
+//        sliderView.setIndicatorUnselectedColor(R.attr.colorPrimary);
+        sliderView.setScrollTimeInSec(3);
+        sliderView.setAutoCycle(true);
+        sliderView.startAutoCycle();
+    }
+}
